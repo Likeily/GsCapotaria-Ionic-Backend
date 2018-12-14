81987e55a2447c130fd9f6f4cc7d7836553803b5
Likeily Medeiros <likeilyms@gmail.com>
2018-12-14 17:53:51 -0200

    Testando a 1 classe de dominio - Categoria

--- src/main/java/br/com/gscapotaria/resources/CategoriaResources.java
+++ src/main/java/br/com/gscapotaria/resources/CategoriaResources.java
@@ -1,16 +1,30 @@
 package br.com.gscapotaria.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gscapotaria.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+    List<Categoria> list = new ArrayList<>();
+    list.add(cat1);
+    list.add(cat2);
+    
+	return list;    		
+	//	return "Rest está funcionando";
 	}
 	
 }
