080f02388968027b9d07e371e409d7b864b3c063
Likeily Medeiros <likeilyms@gmail.com>
2018-12-17 19:16:13 -0200

    Produto e associação n*n

--- src/main/java/br/com/gscapotaria/GscapotariaApplication.java
+++ src/main/java/br/com/gscapotaria/GscapotariaApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.gscapotaria.domain.Categoria;
+import br.com.gscapotaria.domain.Produto;
 import br.com.gscapotaria.repositories.CategoriaRepository;
+import br.com.gscapotaria.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class GscapotariaApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GscapotariaApplication.class, args);
@@ -29,7 +33,16 @@ public class GscapotariaApplication implements CommandLineRunner{
 		Categoria cat4 = new Categoria(null, "Poltronas");
 		Categoria cat5 = new Categoria(null, "Cadeiras");
 		
+		Produto p1 = new Produto(null, "Sofás tradicionais", 500.00);
+		Produto p2 = new Produto(null, "Sofás retráteis", 1000.00);
+		Produto p3 = new Produto(null, "Sofás Divã", 800.00);
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1,cat5));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/br/com/gscapotaria/repositories/CategoriaRepository.java
+++ src/main/java/br/com/gscapotaria/repositories/CategoriaRepository.java
@@ -1,4 +1,5 @@
 package br.com.gscapotaria.repositories;
+
 //DAO acesso ao banco de dados atraves de requisições.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,8 +7,6 @@ import org.springframework.stereotype.Repository;
 import br.com.gscapotaria.domain.Categoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
-	
-	
 }
