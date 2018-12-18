439777215021b152015eb736bae6632f26cb074d
Likeily Medeiros <likeilyms@gmail.com>
2018-12-18 19:17:33 -0200

    EndPoint/Clientes/IdDisponivel

--- src/main/java/br/com/gscapotaria/repositories/CategoriaRepository.java
+++ src/main/java/br/com/gscapotaria/repositories/CategoriaRepository.java
@@ -1,6 +1,5 @@
 package br.com.gscapotaria.repositories;
 
-//DAO acesso ao banco de dados atraves de requisições.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
