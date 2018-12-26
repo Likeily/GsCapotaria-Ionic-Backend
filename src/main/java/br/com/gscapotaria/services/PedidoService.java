d0da2dc6235b87cae580152f0444c7b3f1b125e8
Likeily Medeiros <likeilyms@gmail.com>
2018-12-26 16:45:21 -0200

    Endpoint/Pedidos/{Id} disponibilizado

---
+++ src/main/java/br/com/gscapotaria/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.com.gscapotaria.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gscapotaria.domain.Pedido;
+import br.com.gscapotaria.repositories.PedidoRepository;
+import br.com.gscapotaria.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+	
+}
