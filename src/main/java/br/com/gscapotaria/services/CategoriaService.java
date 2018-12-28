f096a8a2044114a79988d0e832cebcc4f49cff83
Likeily Medeiros <likeilyms@gmail.com>
2018-12-28 12:35:07 -0200

    Deletando uma Categoria com DELETE

--- src/main/java/br/com/gscapotaria/services/CategoriaService.java
+++ src/main/java/br/com/gscapotaria/services/CategoriaService.java
@@ -3,16 +3,18 @@ package br.com.gscapotaria.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.gscapotaria.domain.Categoria;
 import br.com.gscapotaria.domain.Cliente;
 import br.com.gscapotaria.repositories.CategoriaRepository;
+import br.com.gscapotaria.services.exceptions.DataIntegrityException;
 import br.com.gscapotaria.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
- 
+
 	@Autowired
 	private CategoriaRepository repo;
 
@@ -20,16 +22,25 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		
+
 	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivél excluir uma Categoria que possui produtos");
+		}
 	}
 }
