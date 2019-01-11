bac03abf86a321405f8812592dca6271278b3bed
Likeily Medeiros <likeilyms@gmail.com>
2019-01-11 19:33:17 -0200

    Listando todas categorias com DTO

---
+++ src/main/java/br/com/gscapotaria/dto/CategoriaDTO.java
@@ -0,0 +1,41 @@
+package br.com.gscapotaria.dto;
+
+import java.io.Serializable;
+
+import br.com.gscapotaria.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+//definição de dados ao trafegar em operações básicas de categorias
+	
+	private Integer id; 
+	private String nome;
+
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
+
--- src/main/java/br/com/gscapotaria/resources/CategoriaResource.java
+++ src/main/java/br/com/gscapotaria/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.gscapotaria.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.gscapotaria.domain.Categoria;
+import br.com.gscapotaria.dto.CategoriaDTO;
 import br.com.gscapotaria.services.CategoriaService;
 
 @RestController
@@ -37,7 +40,7 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
@@ -48,5 +51,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+
 	}
 }
